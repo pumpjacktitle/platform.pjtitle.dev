9f521c2c04c36b7403a0ae28b6bf18019796e8c0
Daniel Berry <daniel.berry@pjtitle.com>
2014-06-06 16:48:58 -0500

    updates dev env database config
    Signed-off-by: Daniel Berry <daniel.berry@pjtitle.com>

--- app/config/local/database.php
+++ app/config/local/database.php
@@ -22,26 +22,14 @@ return array(
 
 		'mysql' => array(
 			'driver'    => 'mysql',
-			'host'      => 'localhost',
-			'database'  => 'homestead',
-			'username'  => 'homestead',
-			'password'  => 'secret',
+			'host'      => getenv('DB_HOST'),
+			'database'  => getenv('DB_NAME'),
+			'username'  => getenv('DB_USER'),
+			'password'  => getenv('DB_PASS'),
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
-		),
-
-		'pgsql' => array(
-			'driver'   => 'pgsql',
-			'host'     => 'localhost',
-			'database' => 'homestead',
-			'username' => 'homestead',
-			'password' => 'secret',
-			'charset'  => 'utf8',
-			'prefix'   => '',
-			'schema'   => 'public',
-		),
-
+		)
 	),
 
 );
